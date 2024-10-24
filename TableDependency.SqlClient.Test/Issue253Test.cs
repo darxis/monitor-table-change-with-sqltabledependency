cccffe7b7b0abe2b6571904c38f5f911f6d6bb68
Alexis Nowikowski <a.nowikowski@blaxdev.com>
2024-10-24 13:53:37 +0200

    Added test for issue #253

---
+++ TableDependency.SqlClient.Test/Issue253Test.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TableDependency.SqlClient.Test
+{
+    [TestClass]
+    public class Issue253Test : Base.SqlTableDependencyBaseTest
+    {
+        private class Issue253Model
+        {
+            public string Id { get; set; }
+        }
+
+        private static readonly string TableName = nameof(Issue253Model);
+
+        private Exception _unobservedTaskException;
+
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext testContext)
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"IF OBJECT_ID('[{TableName}]', 'U') IS NOT NULL DROP TABLE [dbo].[{TableName}]";
+                    sqlCommand.ExecuteNonQuery();
+                    sqlCommand.CommandText = $"CREATE TABLE [{TableName}]([Id] [int] NULL)";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            using (var sqlConnection = new SqlConnection(ConnectionStringForTestUser))
+            {
+                sqlConnection.Open();
+                using (var sqlCommand = sqlConnection.CreateCommand())
+                {
+                    sqlCommand.CommandText = $"IF OBJECT_ID('{TableName}', 'U') IS NOT NULL DROP TABLE [{TableName}];";
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+        }
+
+        [TestCategory("SqlServer")]
+        [TestMethod]
+        public void Test()
+        {
+            try
+            {
+                using (var tableDependency = new SqlTableDependency<Issue253Model>(ConnectionStringForTestUser, tableName: TableName))
+                {
+                    tableDependency.OnChanged += (o, args) => { };
+                    tableDependency.Start();
+
+                    Thread.Sleep(5000);
+
+                    TaskScheduler.UnobservedTaskException += TaskSchedulerOnUnobservedTaskException;
+                    try
+                    {
+                        tableDependency.Stop();
+
+                        Thread.Sleep(5000);
+
+                        GC.Collect();
+                        GC.WaitForPendingFinalizers();
+                    }
+                    finally
+                    {
+                        TaskScheduler.UnobservedTaskException -= TaskSchedulerOnUnobservedTaskException;
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Assert.Fail(exception.Message);
+            }
+
+            Assert.IsNull(_unobservedTaskException);
+        }
+
+        private void TaskSchedulerOnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            _unobservedTaskException = e.Exception;
+        }
+    }
+}
